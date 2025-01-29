792397008f806ca3ddbfe8551aafc4e219d8f58e
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-01-29 14:05:46 +0530

    Add files via upload

---
+++ largest3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,num3;
+  printf("Enter Three Numbers: ");
+  scanf("%d%d%d",&num1,&num2,&num3);
+  if (num1>num2){
+    if (num1>num3){
+      printf("Largest Number is:%d",num1);
+    }
+  }
+  else if (num2>num1){
+    if(num2>num3){
+      printf("Largest Number is:%d",num2);
+    }
+  }
+  else if(num3>num1){
+    if (num3>num2){
+      printf("Largest Number is:%d",num3);
+    }
+  }
+  else{
+    printf("All numbers are equal");
+  }
+  
+  return 0;
+}
