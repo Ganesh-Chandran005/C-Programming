3951f65d8c50ca7279db8100ebfca1fc7f1967b4
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-04-09 15:08:39 +0530

    Add files via upload

--- file.c
+++ file.c
@@ -1,15 +1,40 @@
 #include<stdio.h>
-int main(){
-  FILE *fp1,fp2,fp3;
-  fp1=fopen("num.txt","r");
+void readFile(FILE *fp){
   int num;
-  fscanf(fp1,"%d",&num);
-  while(!feof(fp1)){
+  while(fscanf(fp,"%d",&num)==1){
     printf("%d\t",num);
-    fscanf(fp1,"%d",&num);
-   
+  }
+  fclose(fp);
+}
+  
+int main(){
+  FILE *fp1,*fp2,*fp3;
+  fp1=fopen("num.txt","r");
+  fp2=fopen("odd.txt","w");
+  fp3=fopen("even.txt","w");
+  int num;
+  while(fscanf(fp1,"%d",&num)==1){
+    if(num%2==0){
+      fprintf(fp3,"%d\t",num);
+      }
+    else{
+      fprintf(fp2,"%d\t",num);
+    }
   }
   fclose(fp1);
+  fclose(fp2);
+  fclose(fp3);
+  
+  printf("\nThe Input Text File Contents: \n");
+  fp1=fopen("num.txt","r");
+  readFile(fp1);
+  printf("\nThe Odd number file contents: \n");
+  fp2=fopen("odd.txt","r");
+  readFile(fp2);
+  printf("\nThe Even number file contents: \n");
+  fp3=fopen("even.txt","r");
+  readFile(fp3);
+  
   return 0;
 }
   
